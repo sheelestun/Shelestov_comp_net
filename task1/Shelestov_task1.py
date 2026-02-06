94f071a26529a35dc0376db73079d0471eb9b57b
sheelestun <sheelestun@gmail.com>
2026-02-06 15:14:23 +0700

    added task1

---
+++ task1/Shelestov_task1.py
@@ -0,0 +1,35 @@
+import csv
+from icmplib import ping
+
+domains = ["google.com", "yandex.ru", "github.com", "rutracker-net.ru",
+           "vk.com", "mail.ru", "youtube.com", "nsu.ru", "nstu.ru"]
+results = []
+for domain in domains:
+    try:
+        host = ping(domain, count=3, timeout=2, privileged=True)
+        results.append({
+            "Host": domain,
+            "RTT": host.avg_rtt,
+            "Jitter": host.jitter,
+            "Packet Loss%": int(host.packet_loss * 100),
+            "Packets Received": host.packets_received
+        })
+        print(f"Checked: {domain}")
+    except Exception as e:
+        results.append({
+            "Host": domain,
+            "RTT": "-",
+            "Jitter": "-",
+            "Packet Loss%": 100,
+            "Packets Received": 0
+        })
+        print(f"Error during checking domain: {domain}!\n "
+              f"Error: {e}")
+
+with open("ping_results.csv", "w", newline="") as f:
+    result = csv.DictWriter(f, fieldnames=["Host", "RTT", "Jitter",
+                                           "Packet Loss%", "Packets Received"],
+                            delimiter=";")
+    result.writeheader()
+    result.writerows(results)
+print("\nResults saved in ping_results.csv")
